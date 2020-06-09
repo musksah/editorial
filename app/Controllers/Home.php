148bae6d63f2f8e3bc94b895524ad6628e193dec
Sebastián Huérfano <xsebastianhrodriguezx@gmail.com>
2020-06-08 21:47:33 -0500

    Arreglo headers 2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,9 +1,5 @@
 <?php namespace App\Controllers;
 
-header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Headers', '*');
-header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-
 class Home extends BaseController
 {
 	public function index()
